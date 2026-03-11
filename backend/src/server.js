10b1fa0317a35552fcf18c54169359ea973aec1a
imranazizi <imranazizi.0066@gmail.com>
2026-03-11 15:43:31 +0430

    solve some problem that occured in backend folder

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,10 @@
-import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
-
+import dotenv from 'dotenv';
+// Load environment variables
+dotenv.config();
 // Connect to MongoDB
 connectDB();
 
